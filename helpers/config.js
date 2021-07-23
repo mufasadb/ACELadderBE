e93aad9c4dbec32380b568492bd1b90f6825732f
Daniel Beach <dtmhawk@gmail.com>
2021-07-23 12:12:11 +1000

    updated league names

--- helpers/config.js
+++ helpers/config.js
@@ -2,10 +2,15 @@
 
 const config = {
     timeout: 5,
-    leagueList :["Ultimatum",
-        "Hardcore Ultimatum",
-        "SSF Ultimatum",
-        "SSF Ultimatum HC"],
+    // leagueList :["Ultimatum",
+    //     "Hardcore Ultimatum",
+    //     "SSF Ultimatum",
+    //     "SSF Ultimatum HC"],
+    leagueList :["Expedition",
+        "Hardcore Expedition",
+        "SSF Expedition",
+        "SSF Expedition HC"],
+
     currentAddress: "http://ec2-13-211-237-68.ap-southeast-2.compute.amazonaws.com/"
 }
 module.exports = {
